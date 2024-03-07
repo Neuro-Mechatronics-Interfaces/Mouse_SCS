731b59f2987973fb02b176fe92eff71142750a7a
Max (Lenovo) <max.murphy11@gmail.com>
2024-03-07 13:18:30 -0500

    Updates from student office.

--- plot_saga_recruitment_raw.m
+++ plot_saga_recruitment_raw.m
@@ -6,6 +6,21 @@ arguments
     channel
     options.SampleRate = 4000;
     options.Tag {mustBeTextScalar} = "SAGA";
+    options.TriggerChannel (1,1) double {mustBePositive, mustBeInteger} = 73;
+    options.SyncBit (1,1) double {mustBeInteger} = 12;
+    options.Fc (1,:) double = 5;
+    options.YLim (1,2) double = [-40 40]; % microamps
+end
+
+if numel(options.Fc) == 1
+    [b,a] = butter(2,options.Fc/(options.SampleRate/2),'high');
+    applyFilter = true;
+elseif numel(options.Fc) == 2
+    [b,a] = butter(2,options.Fc./(options.SampleRate/2), 'bandpass');
+    applyFilter = true;
+else
+    applyFilter = false;
+
 end
 
 [~,idx] = sort(abs(T.intensity), 'ascend');
@@ -20,11 +35,20 @@ L = tiledlayout(fig, ceil(N/2), 2);
 for ii = 1:N
     ax = nexttile(L, ii, [1 1]);
     set(ax, 'NextPlot', 'add', 'FontName', 'Tahoma');
-    data = zscore(saga(ii).samples(channel,:));
+    if applyFilter
+        data = filter(b,a,saga(ii).samples(channel,:));
+    else
+        data = saga(ii).samples(channel,:);
+    end
     t = (0:(numel(data)-1)) / options.SampleRate;
+    yyaxis(ax, 'left');
     plot(ax, t, data);
-    
+    title(ax, sprintf('%d \\muA', abs(T.intensity(ii))), 'FontName', 'Tahoma', 'Color', 'k');
+    ylim(ax, options.YLim);
+    yyaxis(ax, 'right');
+    plot(ax, t, bitand(saga(ii).samples(options.TriggerChannel,:),2^options.SyncBit)==0);
 end
 title(L, sprintf('Sweep %d | %s | Channel-%d', T.sweep(1), options.Tag, channel), ...
     'FontName', 'Tahoma', 'Color', 'k');
+linkaxes(findobj(L.Children,'Type','axes'),'xy');
 end
